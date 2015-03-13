bedf475230129ec4aa332014e3f55b8f8fa6e398
googyanas <googyanas@gmail.com>
2015-03-13 18:03:48 +0000

    workqueue: fix compilation error

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -3925,7 +3925,7 @@ static int __init init_workqueues(void)
 					      0);
 	BUG_ON(!system_wq || !system_long_wq || !system_nrt_wq ||
 	       !system_unbound_wq || !system_freezable_wq ||
-	       !system_nrt_freezable_wq) ||
+	       !system_nrt_freezable_wq ||
 	       !system_power_efficient_wq ||
 	       !system_freezable_power_efficient_wq);
 	return 0;
